7c2ce43ed168174865ee4c788b34261fb9fa0053
dp0s <dps@my.mail.de>
2020-03-23 02:35:26 +0100

    Update setup.py to create 2 seperate packages

--- setup.py
+++ setup.py
@@ -1,37 +1,67 @@
 from setuptools import setup, find_packages
+import shutil
+from time import sleep
+import multiprocessing
+
+version_number = "0.0.3c1"
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-setup(
-    name="Dpowers",
-    version="0.0.2",
-    packages=find_packages("Dlib"),
-    package_dir={"": "Dlib"},
-
-	#package_data={
-        # If any package contains *.json files, include them:
-     #   "": ["Dlib/Dhelpers/KeyboardLayouts/layouts_imported_from_xkb/*",
-      #      "Dlib/Dpowers/iconpower/icons"]},
-    include_package_data=True,
-	
-    install_requires=["pynput>=1.6.8", "pystray>=0.15",
-        "evdev==0.7"],
     
-    python_requires=">=3.6",
-
-    # metadata to display on PyPI
-    author="DPS",
-    author_email="dps@my.mail.de",
-    url="https://github.com/dp0s/Dpowers",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-    ],
     
+
+
+def my_setup(name, **kwargs):
+    shutil.rmtree(f"Dlib/{name}.egg-info", ignore_errors=True)
+    shutil.rmtree("build", ignore_errors=True)
+    print()
+    print()
+    print(f"------------------------  Package {name} ----------------------------")
+    print()
+    sleep(1)
+    # for creating wheels using bdist_wheel it is somehow necessary to run
+    # the setup command inside a seperate process.
+    sub_process = multiprocessing.Process(target=setup_process, args=(name,),
+            kwargs=kwargs)
+    sub_process.start()
+    sub_process.join()
+
+def setup_process(name, **kwargs):
+    setup(**kwargs,
+        name=name,
+        packages=find_packages("Dlib", include=[f"{name}*"]),
+        package_dir={"": "Dlib"},
+        version=version_number,
+        include_package_data=False,
+        ####must stay disabled to allow package_data option !!!
+        python_requires=">=3.6", # metadata to display on PyPI
+        author="DPS", author_email="dps@my.mail.de",
+        url="https://github.com/dp0s/Dpowers",
+        classifiers=["Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
+         )
+
+
+
+
+
+my_setup("Dpowers",
+    package_data = {"Dpowers.iconpower.icons" : ["*"]},
+
+    install_requires=["pynput>=1.6.8", "pystray>=0.15", f"Dhelpers=="
+                f"{version_number}"],
+
     description="Unified Interface for automatic interaction",
     long_description=long_description,
     long_description_content_type="text/markdown"
+        )
 
-    # could also include long_description, download_url, etc.
-)
+
+
+my_setup("Dhelpers",
+        package_data = {"Dhelpers.KeyboardLayouts.layouts_imported_from_xkb"
+        : ["*"]},
+        install_requires=["evdev==1.2.0", "psutil>=5.4.2"],
+        description="Dhelpers support modules"
+        )
