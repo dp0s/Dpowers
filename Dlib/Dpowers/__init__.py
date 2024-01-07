5f25c8ec869e4f1d0c26c697745f1d8fd4cbc516
dp0s <dp0s@e.mail.de>
2024-01-07 21:13:48 +0100

    v0.1.5rc2

--- Dlib/Dpowers/__init__.py
+++ Dlib/Dpowers/__init__.py
@@ -39,7 +39,7 @@ except ModuleNotFoundError:
     import Dhelpers
     
 
-__version__ = "0.1.5rc1"
+__version__ = "0.1.5rc2"
 #Dpowers and Dhelpers share version number
 if __version__ != Dhelpers.__version__:
     import warnings
